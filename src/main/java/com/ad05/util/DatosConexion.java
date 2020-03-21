369abcc171c45227d8e546d8d409bccb7762b235
Manuel <manuelalvarezperez88@yahoo.es>
2020-03-21 16:23:51 +0100

    ya podemos insertar las carpetas en la BD

---
+++ src/main/java/com/ad05/util/DatosConexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ad05.util;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Manuel
+ */
+public class DatosConexion {
+    
+    
+    private Map<String, String> dbConnection;
+    private Map<String, String> app;
+
+    public Map<String, String> getDbConnection() {
+        return dbConnection;
+    }
+
+    public Map<String, String> getApp() {
+        return app;
+    }
+}
